3ec0ec5b3f76ba18f6cfdfeb99dbd79d90bbdfd9
Brytton Tsai <brytton.tsai@mail.utoronto.ca>
2021-11-02 18:34:31 -0700

    Finished login.html task

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,13 @@
+
+// Firebase API Configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC0NqGL8JX3I3u3-Z2-YOKpZd8e4S0ra0M",
+    authDomain: "bby22-comp1800.firebaseapp.com",
+    projectId: "bby22-comp1800",
+    storageBucket: "bby22-comp1800.appspot.com",
+    messagingSenderId: "534749606096",
+    appId: "1:534749606096:web:e2641d7bd3d1fca59beed5"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
