a4fbca064e84edef5b84dd83b2cad04c8fbba814
benjamin.chen <bchen109@my.bcit.ca>
2021-11-08 08:12:56 -0800

    Ben_008: update post (work in progress still)

--- scripts/post.js
+++ scripts/post.js
@@ -7,14 +7,41 @@ var toolbarOptions = [
   [{ 'font': ['calibri', 'arial', 'times-new-roman', 'verdana'] }],
   ['bold', 'italic', 'underline'],
   [{ 'color': [] }, { 'background': [] }],
-  ['link', 'image'],
+  ['link'],
   [{ 'align': [] }],
   [{ 'list': 'ordered'}, { 'list': 'bullet' }]
 ]
 
 var quill = new Quill('#editor', {
+  theme: 'snow',
   modules: {
     toolbar: toolbarOptions
   },
-  theme: 'snow'
 });
+
+// data
+let = file = {};
+document.getElementById('fileUploader').addEventListener('change', function (e) {
+  file = e.target.files[0];
+})
+
+document.querySelector('#submit').addEventListener("click", function(e) {
+  e.preventDefault();
+  var myEditor= document.querySelector('#editor');
+  var html = myEditor.children[0].innerHTML;
+  console.log(html);
+
+  firebase.auth().onAuthStateChanged(user => {
+
+    if (user) {
+      console.log(user.uid);
+
+      // Get A Profile Image from the Cloud Storage
+      firebase
+      .storage()
+      .ref("users")
+      .child(user.uid + "/post.jpg")
+      .put(file);
+    }
+  })
+})
